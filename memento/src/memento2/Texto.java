088440d775c4a24b250fcde636dfb4db359cedf8
TansenSilva <tansensilva@gmail.com>
2022-08-17 14:58:31 -0300

    exemplos padrao memento

---
+++ memento/src/memento2/Texto.java
@@ -0,0 +1,22 @@
+package memento2;
+
+public class Texto {
+    protected String texto;
+    TextoCareTaker caretaker;
+
+    public Texto() {
+        caretaker = new TextoCareTaker();
+        texto = new String();
+    }
+    public void escreverTexto(String novoTexto) {
+        caretaker.adicionarMemento(new TextoMemento(texto));
+        texto += novoTexto;
+    }
+    public void desfazerEscrita() {
+        texto = caretaker.getUltimoEstadoSalvo().getTextoSalvo();
+    }
+    public void mostrarTexto() {
+        System.out.println(texto);
+    }
+
+}
